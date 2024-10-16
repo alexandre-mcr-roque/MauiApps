841502ea64a3a78918dae59adc80b80349cdfeae
Alexandre Roque <alexandre.mcr.roque@gmail.com>
2024-10-16 18:54:21 +0100

    MauiControls

--- MauiControls/App.xaml.cs
+++ MauiControls/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace MauiDemo
+﻿using MauiControls.Pages;
+
+namespace MauiControls
 {
     public partial class App : Application
     {
@@ -6,7 +8,7 @@
         {
             InitializeComponent();
 
-            MainPage = new NavigationPage(new AbsoluteLayoutDemo());
+            MainPage = new NavigationPage(new MauiPresentationControls());
         }
     }
 }
